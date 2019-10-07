d2542344942c5a250043f6229ff689377366c07e
Xemka <43967788+Xemka@users.noreply.github.com>
2019-10-07 18:51:29 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.guessNum = Math.round((this.max + this.min) / 2);
 
+        return this.guessNum;
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
